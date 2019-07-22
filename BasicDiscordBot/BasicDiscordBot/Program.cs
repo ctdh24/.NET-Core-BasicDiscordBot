cb6af04dc7b9047db5a9d12f0f35a6487583f5df
Calvin Huynh <ctdh24@gmail.com>
2019-07-21 21:44:08 -0700

    reformatting, getting rid of wolframalpha, starting on simple reddit implementation

--- BasicDiscordBot/BasicDiscordBot/Program.cs
+++ BasicDiscordBot/BasicDiscordBot/Program.cs
@@ -4,8 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using DSharpPlus;
 using DSharpPlus.CommandsNext;
-using Wolfram.Alpha;
-using Wolfram.Alpha.Models;
 
 namespace BasicDiscordBot
 {
@@ -13,7 +11,6 @@ namespace BasicDiscordBot
     {
         static DiscordClient discord;
         static CommandsNextModule commands;
-        static WolframAlphaRequestHandler wolframAlphaRequestHandler;
 
         static void Main(string[] args)
         {
@@ -37,25 +34,6 @@ namespace BasicDiscordBot
                     await e.Message.RespondAsync("pong!");
                 if (string.Equals(listStrLineElements.ElementAt(0), "hello", StringComparison.OrdinalIgnoreCase))
                     await e.Message.RespondAsync("Hi "+e.Message.Author.Username+"!");
-                //if (string.Equals(listStrLineElements.ElementAt(0), "scrabble", StringComparison.OrdinalIgnoreCase))
-                //{
-                //    WolframAlphaService service = new WolframAlphaService("placeholder");
-                //    WolframAlphaRequest request = new WolframAlphaRequest(listStrLineElements.ElementAt(1) + " scrabble");
-                //    WolframAlphaResult result = await service.Compute(request);
-                //    var scrabbleScore = result.QueryResult.Pods.ElementAt(1);
-                //    foreach (var pod in result.QueryResult.Pods)
-                //    {
-                //        if (pod.SubPods != null)
-                //        {
-                //            Console.WriteLine(pod.Title);
-                //            await e.Channel.SendMessageAsync(pod.Title);
-                //            foreach (var subpod in pod.SubPods)
-                //            {
-                //                await e.Channel.SendMessageAsync("    " + subpod.Plaintext);
-                //            }
-                //        }
-                //    }
-                //}
             };
             commands = discord.UseCommandsNext(new CommandsNextConfiguration
             {
